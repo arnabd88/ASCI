c7793b94901e094edc284f008b88512618608d43
arnabd88 <arnab.d.88@gmail.com>
2016-11-28 06:25:59 -0700

    Intermediate

--- Assignment5_Submit_u1014840/Prob10.m
+++ Assignment5_Submit_u1014840/Prob10.m
@@ -1,44 +1,65 @@
-function [] = q10
+
+function [] = Prob10
+
 
 % Data Set
-X = [0.10, 0.15, 0.20, 0.30, 0.35, 0.50, 0.75];
-Y = [3.00, 2.00, 1.20, 2.10, 2.00, 2.50, 2.50];
+PX = [0.10, 0.15, 0.20, 0.30, 0.35, 0.50, 0.75];
+PY = [3.00, 2.00, 1.20, 2.10, 2.00, 2.50, 2.50];
 
 % Construct Interpolatants
-[p1] = polyfit(X,Y,6);
-[p2] = spline(X,Y);
-[p3] = custom(0.1);
-[p4] = custom(0.01);
-[p5] = custom(0.001);
+van = vander(PX);
+[t1] = van\PY' ;
+[t2] = spline(PX,PY);
+[t3] = interpolant(0.1,PX,PY);
+[t4] = interpolant(0.01,PX,PY);
+[t5] = interpolant(0.001,PX,PY);
 
 % Plot 
-x = [0: 0.01: 0.9];
-y1 = polyval(p1,x);
-y2 = ppval(p2,x); 
-y3 = p3(x);
-y4 = p4(x);
-y5 = p5(x);
-axis([0,10,-50,50])
-xlim([0 1.1])
-plot(x,y1,x,y2,x,y3,x,y4,x,y5);
+x = [0.05: 0.01: 0.8];
+y1 = polyval(t1,x);
+y2 = ppval(t2,x); 
+y3 = t3(x);
+y4 = t4(x);
+y5 = t5(x);
+%axis([0,10,-50,50])
+%xlim([0 1.1])
+%% plot(x,y1,x,y2,x,y3,x,y4,x,y5);
+%% 
+%% 
+%% xlabel('x');
+%% ylabel('y');
+%% hold on;
+%% legend('polyfit','spline','interpolant 0.1','interpolant 0.01','interpolant 0.001', 'Location', 'southeast');
 
 
-xlabel('x');
-ylabel('y');
-hold on;
-legend('polyfit','spline','custom 0.1','custom 0.01','custom 0.001');
+subplot(3,2,1);
+plot(x,y1);
+title(['Poly Interpolant']), xlabel('x'), ylabel('y');
 
-line([0.05,0.05],[-5,15]); hold on;
-line([0.01,0.01],[-5,15]); hold on;
-line([0.8,0.8],[-5,15]);
+subplot(3,2,2);
+plot(x,y2);
+title(['Cubic Spline']), xlabel('x'), ylabel('y');
+subplot(3,2,3);
+plot(x,y3);
+title(['Custom: epsilon = 0.1']), xlabel('x'), ylabel('y');
+subplot(3,2,4);
+plot(x,y4);
+title(['Custom: epsilon = 0.01']), xlabel('x'), ylabel('y');
+subplot(3,2,5);
+plot(x,y5);
+title(['Custom: epsilon = 0.001']), xlabel('x'), ylabel('y');
+
+%line([0.05,0.05],[-5,15]); hold on;
+%line([0.01,0.01],[-5,15]); hold on;
+%line([0.8,0.8],[-5,15]);
 
 end
 
-function [p] = custom(e)
+function [p] = interpolant(e,X,Y)
 
   A = ones(8,8);
-  X = [0.10, 0.15, 0.20, 0.30, 0.35, 0.50, 0.75];
-  Y = [3.00, 2.00, 1.20, 2.10, 2.00, 2.50, 2.50];
+ % X = [0.10, 0.15, 0.20, 0.30, 0.35, 0.50, 0.75];
+ % Y = [3.00, 2.00, 1.20, 2.10, 2.00, 2.50, 2.50];
   
   phi0 = @(x) 1 ;
   phi1 = @(x) sqrt((x - X(1)).^2 + e^2) - e ;
